d6bbfcf3dfa700d4f9e0599b9b2437db54fa0010
Manuel Montoya <manuwhs@gmail.com>
2020-10-19 15:27:01 +0200

    Cannot remember if I pushed everything back in the day

--- thothsnehet/concepts/word_node.py
+++ thothsnehet/concepts/word_node.py
@@ -6,13 +6,17 @@ class Netword():
     
     def add_node(word, parent):
         node = WordNode(word, parent)
+
+        # If the parent exists:
         if parent in word_to_node.keys():
             parent_node = word_to_node[parent]
             parent_node.children.append(node)
         else:
+            # If it does not exist, then add it to the peaks 
             tree_peaks.append(word)
         word_to_node[word] = node
 
+    
 class WordNode():
     def __init__(word, parent):
         self.word = word
